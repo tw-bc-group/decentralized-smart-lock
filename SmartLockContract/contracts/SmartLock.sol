c1f0d40d3913cd94200f774c98cb922798f2663c
RuipengWang <rpwang@Edwards-MacBook-Pro.local>
2018-01-28 06:24:46 +0800

    Change new version -- Ruipeng Wang

--- SmartLockContract/contracts/SmartLock.sol
+++ SmartLockContract/contracts/SmartLock.sol
@@ -3,16 +3,13 @@ pragma solidity ^0.4.15;
 import "./Decode.sol";
 
 contract SmartLock {
-	struct SmartLockContract {
-		address landlord;
-		bytes32 lockAddress;
-		uint256 rentMoneyPerDay;
-		address renter;
-		uint256 totalRentMoneyFromRenter;
-		uint256 lastDate;
-	}
+	address landlord;
+	bytes32 lockAddress;
+	uint256 rentMoneyPerDay;
+	address renter;
+	uint256 totalRentMoneyFromRenter;
+	uint256 lastDate;
 	
-	SmartLockContract slContract;
 
 	event RegisterLandlord(address landlord, bytes32 lockAddress, uint256 rentMoneyPerDay);
 	event WantToRent(address renter, uint256 totalRentMoneyFromRenter, uint256 lastDate);
@@ -39,35 +36,33 @@ contract SmartLock {
 		_; 
 	}
 
-	function isLandlord(address landlord) constant returns(bool res) {
-		return slContract.landlord == landlord;
+	function isLandlord(address landlordAddr) constant returns(bool res) {
+		return landlord == landlordAddr;
 	}
 	
 	function isLockAvailiable() constant returns(bool res) {
-		return slContract.lastDate < now;
+		return lastDate < now;
 	}
 
 	function amIRentedThisRoom() constant returns(bool res) {
 		address addressNeedToVerify = msg.sender;
-		return addressNeedToVerify == slContract.renter;
+		return addressNeedToVerify == renter;
 	}
 
 	function getRentMoneyPerDay() constant returns(uint256 rentMoneyPerDay) {
-		return slContract.rentMoneyPerDay;
+		return rentMoneyPerDay;
 	}
 
-	function registerLandlord(bytes32 lockAddress, uint256 rentMoneyPerDay) {
-		address landlord = msg.sender;
-		slContract = SmartLockContract({
-				landlord: landlord,
-				lockAddress: lockAddress,
-				rentMoneyPerDay: rentMoneyPerDay,
-				renter: landlord,
-				totalRentMoneyFromRenter: 0,
-				lastDate: 0
-			});
+	function registerLandlord(bytes32 lockAddr, uint256 rentMoneyPD) {
+		address landlordAddr = msg.sender;
+		landlord = landlordAddr;
+		lockAddress = lockAddr;
+		rentMoneyPerDay = rentMoneyPD;
+		renter = landlordAddr;
+		totalRentMoneyFromRenter = 0;
+		lastDate = 0;
 
-		RegisterLandlord(landlord, lockAddress, rentMoneyPerDay);
+		RegisterLandlord(landlordAddr, lockAddr, rentMoneyPD);
 	}
 
 	function() payable {
@@ -75,33 +70,33 @@ contract SmartLock {
 	}
 
 	function wantToRent() onlyLockIsAvailable notLandlord(msg.sender) payable {
-		address renter = msg.sender;
-		uint256 totalRentMoneyFromRenter = msg.value;
+		address renterAddr = msg.sender;
+		uint256 totalRentMoneyFromRenterSide = msg.value;
 
-		slContract.renter = renter;
-		slContract.totalRentMoneyFromRenter = totalRentMoneyFromRenter;
-		slContract.lastDate = now + (totalRentMoneyFromRenter / slContract.rentMoneyPerDay) * 1 days;
+		renter = renterAddr;
+		totalRentMoneyFromRenter = totalRentMoneyFromRenterSide;
+		lastDate = now + (totalRentMoneyFromRenterSide / rentMoneyPerDay) * 1 days;
 
-		WantToRent(slContract.renter, slContract.totalRentMoneyFromRenter, slContract.lastDate);
+		WantToRent(renter, totalRentMoneyFromRenter, lastDate);
 	}
 
 	function canIOpenThisDoor(bytes memory sha3Message, bytes memory signedStr) constant returns(bool res) {
-		return Decode.decode(sha3Message, signedStr) == slContract.renter && now < slContract.lastDate;
+		return Decode.decode(sha3Message, signedStr) == renter && now < lastDate;
 	}
 	
 	function transferRentMoney() onlyLandlord(msg.sender) onlyLockIsAvailable{
 		address landlord = msg.sender;
-		landlord.transfer(slContract.totalRentMoneyFromRenter);
+		landlord.transfer(totalRentMoneyFromRenter);
 
-		TransferRentMoney(landlord, slContract.totalRentMoneyFromRenter);
+		TransferRentMoney(landlord, totalRentMoneyFromRenter);
 
 		clearContract();
 	}
 	
 	function clearContract() private returns(bool res) {
-		slContract.totalRentMoneyFromRenter = 0;
-		slContract.renter = msg.sender;
-		slContract.lastDate = 0;
+		totalRentMoneyFromRenter = 0;
+		renter = msg.sender;
+		lastDate = 0;
 
 		ClearContract(msg.sender, now);
 	}

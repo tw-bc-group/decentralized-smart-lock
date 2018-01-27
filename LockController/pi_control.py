981f33877321131a6094bcda78c233fbda5da296
mengfan <mengfan@kitty.null>
2018-01-28 02:19:38 +0800

    add try catch

--- LockController/pi_control.py
+++ LockController/pi_control.py
@@ -6,13 +6,16 @@ from time import sleep
 
 
 while True:
-    codes = pi_camera.capture_img()
-    args = json.loads(codes, encoding='utf8')
+    try:
+        codes = pi_camera.capture_img()
+        args = json.loads(codes, encoding='utf8')
 
-    print('check can open door, args: %s' % codes)
-    if contract.can_open_door(args):
-        print('open door ...')
-        lock_control.open_lock()
-        sleep(3)
-    else:
-        print('no premission ...')
+        print('check can open door, args: %s' % codes)
+        if contract.can_open_door(args):
+            print('open door ...')
+            lock_control.open_lock()
+            sleep(3)
+        else:
+            print('no premission ...')
+    except:
+        print('some error ...')

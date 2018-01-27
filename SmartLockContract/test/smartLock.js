a4e48478edbe60800103497e7839aa7b5633433b
RuipengWang <rpwang@Edwards-MacBook-Pro.local>
2018-01-27 14:04:44 +0800

    Add smart contract test first -- Ruipeng Wang

---
+++ SmartLockContract/test/smartLock.js
@@ -0,0 +1,27 @@
+var smartLock = artifacts.require("./SmartLock.sol");
+
+contract('SmartLock', function(accounts){
+	it("landlord should register successfully with his or her lock address and rent money", function(){
+		var smartLock;
+		var landlord = accounts[0];
+		var lockAddress = "lock_address";
+		var rentMoneyPerDay = web3.toWei(100,'ether');
+
+		return SmartLock.new().then(function(instance){
+			smartLock = instance;
+			return smartLock.registerLandlord(lockAddress, rentMoneyPerDay, {from: landlord});
+		}).then(function(){
+			return smartLock.isLandlord(landlord, lockAddress);
+		}).then(function(res){
+			assert.equal(res.valueOf(), true, "if landlord has registered successfully.");
+		}).then(function(){
+			return smartLock.isLockAvaliable(lockAddress);
+		}).then(function(res){
+			assert.equal(res.valueOf(), true, "if lock has been registered and avaliable.");
+		}).then(function(){
+			return smartLock.getRentMoneyPerDay(lockAddress);
+		}).then(function(res){
+			assert.equal(res.valueOf(), rentMoneyPerDay, "if rent money has been set.")
+		});
+	});
+});

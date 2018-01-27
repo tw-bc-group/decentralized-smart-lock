6a2b2ebe94db5f9d0b225a9feb8c6cb4f1ba9e53
dqisme <dqisme@outlook.com>
2018-01-27 17:50:14 +0800

    [dq] try to connect blockchain.

--- Mobile/src/screens/HomeScreen.js
+++ Mobile/src/screens/HomeScreen.js
@@ -6,6 +6,8 @@ import {
   StyleSheet,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import Web3 from 'web3';
+
 import { ROUTE_NAMES } from '../routes';
 
 const styles = StyleSheet.create({
@@ -16,16 +18,37 @@ const styles = StyleSheet.create({
   },
 });
 
-function HomeScreen(props) {
-  return (
-    <View style={styles.container}>
-      <Text>This is Home Screen.</Text>
-      <Button
-        title="Generate QR Code"
-        onPress={() => props.navigation.navigate(ROUTE_NAMES.QR_CODE)}
-      />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  state = {
+    connection: 'Loading ...',
+  };
+
+  componentDidMount() {
+    this.web3.eth.net.isListening().then((isConnected) => {
+      this.setState({
+        connection: isConnected ?
+          'The Blockchain is online!' : 'Oops..something wrong with connection',
+      });
+    });
+  }
+
+  host = 'http://10.16.82.48:8545';
+  web3 = new Web3(new Web3.providers.HttpProvider(this.host));
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is Home Screen.</Text>
+        <Text>
+          {this.state.connection}
+        </Text>
+        <Button
+          title="Generate QR Code"
+          onPress={() => this.props.navigation.navigate(ROUTE_NAMES.QR_CODE)}
+        />
+      </View>
+    );
+  }
 }
 
 HomeScreen.propTypes = {
